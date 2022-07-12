4b0efecd8b637b9e794120f02ee6e006ae3857b5
Salman <salmankarimzindani@hotmail.com>
2022-07-12 12:08:32 -0500

    feature: creates basic being and person class and updates code to interact with the two classes

--- src/OopPlayground/Program.cs
+++ src/OopPlayground/Program.cs
@@ -10,6 +10,17 @@ namespace OopPlayground
     {
         static void Main(string[] args)
         {
+            var b = new Being();
+            Console.WriteLine($"this is b: {b}");
+
+            var p1 = new Person();
+            p1.Name = "Salman";
+
+            var p2 = new Person();
+            p2.Name = "Eman";
+
+            Console.WriteLine($"this is p1.Name: {p1.Name}");
+            Console.WriteLine($"this is p2.Name: {p2.Name}");
         }
     }
 }
