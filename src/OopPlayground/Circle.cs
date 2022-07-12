6b16eeeaedd9214e53ace71584a6eda1bc5f1ed9
Salman <salmankarimzindani@hotmail.com>
2022-07-12 12:22:49 -0500

    feature: creates a Circle class and updates Program code to derive a circle's area given an arbitrary radius

---
+++ src/OopPlayground/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopPlayground
+{
+    internal class Circle
+    {
+        private int radius;
+
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public double Area()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
--- src/OopPlayground/Program.cs
+++ src/OopPlayground/Program.cs
@@ -14,6 +14,8 @@ namespace OopPlayground
             Console.WriteLine($"this is b: {b}");
 
             var p1 = new Person();
+            // Person.Name is a public property with public setters, therefore we
+            // can explictly update the value outside of its class.
             p1.Name = "Salman";
 
             var p2 = new Person();
@@ -21,6 +23,13 @@ namespace OopPlayground
 
             Console.WriteLine($"this is p1.Name: {p1.Name}");
             Console.WriteLine($"this is p2.Name: {p2.Name}");
+
+            Circle circle = new Circle();
+            // Circle.Radius is a private field which does not allow its value to be EXPLICITLY updated
+            // outside of the Circle class. Updating its value requires us to use the following public method 
+            circle.SetRadius(5);
+
+            Console.WriteLine($"this is the circle's area: {circle.Area()}");
         }
     }
 }
