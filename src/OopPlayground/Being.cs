e6f1df3e2bc2a7bbd13f67d45849b57a987d18df
Salman <salmankarimzindani@hotmail.com>
2022-07-12 13:58:47 -0500

    Enhancement: updates the being class to contain overloaded constructors

--- src/OopPlayground/Being.cs
+++ src/OopPlayground/Being.cs
@@ -8,5 +8,14 @@ namespace OopPlayground
 {
     public class Being
     {
+        public Being()
+        {
+            Console.WriteLine("Being is created");
+        }
+
+        public Being(string being)
+        {
+            Console.WriteLine($"Being {being} is created");
+        }
     }
 }
