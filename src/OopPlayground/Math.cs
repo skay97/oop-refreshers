aee780c1c172e86dd30374ff07b5b659660d04a2
Salman <salmankarimzindani@hotmail.com>
2022-07-27 13:29:34 -0500

    feature: adds PI constant to Math class

---
+++ src/OopPlayground/Math.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopPlayground
+{
+    public class Math
+    {
+        public const double PI = 3.14159265359;
+    }
+}
